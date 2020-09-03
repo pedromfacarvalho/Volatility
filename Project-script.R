13a77a3845fbea5527e4762d270d6851f516f6ac
Pedro Carvalho <pedromfacarvalho@gmail.com>
2020-09-03 19:32:10 +0100

    dados e returns

--- Project-script.R
+++ Project-script.R
@@ -2,8 +2,7 @@
 # Objective : TODO
 # Created by: pedro
 # Created on: 03/09/20
-
-pacman::p_load(quantmod, rugarch, rmgarch)
+pacman::p_load(quantmod, rugarch, rmgarch,coinmarketcapr)
 
 DJI <- getSymbols("DJI", src = "yahoo", from = "2002-01-01", auto.assign = FALSE)
 DJI_adj <- DJI$DJI.Adjusted
@@ -11,3 +10,18 @@ GSPC <- getSymbols("^GSPC", src = "yahoo", from = "2002-01-01", auto.assign = FA
 GSPC_adj <- GSPC$GSPC.Adjusted
 IXIC <- getSymbols("^IXIC", src = "yahoo", from = "2002-01-01", auto.assign = FALSE)
 IXIC_adj <- IXIC$IXIC.Adjusted
+
+
+BTC <- getSymbols("BTC-USD", src = "yahoo", auto.assign = FALSE)
+BTC_adj <- BTC$`BTC-USD.Adjusted`
+ETH <- getSymbols("ETH-USD", src = "yahoo", auto.assign = FALSE)
+ETH_adj <- ETH$`ETH-USD.Adjusted`
+XRP <- getSymbols("XRP-USD", src = "yahoo", auto.assign = FALSE)
+XRP_adj <- XRP$`XRP-USD.Adjusted`
+
+ret_DJI <- dailyReturn(DJI_adj, type = "log")
+ret_GSPC <- dailyReturn(GSPC_adj, type = "log")
+ret_IXIC <- dailyReturn(IXIC_adj, type = "log")
+ret_BTC <- dailyReturn(BTC_adj, type = "log")
+ret_ETH <- dailyReturn(ETH_adj, type = "log")
+ret_XRP <- dailyReturn(XRP_adj, type = "log")
