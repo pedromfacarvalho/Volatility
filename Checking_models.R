b0bb57ce2a5a4561b4482375bd80b1f757598c55
Pedro Carvalho <pedromfacarvalho@gmail.com>
2020-09-05 19:45:47 +0100

    adding articles

---
+++ Checking_models.R
@@ -0,0 +1,74 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: pedro
+# Created on: 04/09/20
+
+
+pacman::p_load(forecast,quantmod, rugarch, rmgarch,coinmarketcapr,xts, tidyverse, ggthemes,
+               gridExtra, tseries, lmtest, FinTS)
+DJI <- getSymbols("DJI", src = "yahoo", from = "2002-01-01", auto.assign = FALSE)
+DJI_adj <- DJI$DJI.Adjusted
+#eGARCH, ARIMA (1,0,0) sstd
+GSPC <- getSymbols("^GSPC", src = "yahoo", from = "2002-01-01", auto.assign = FALSE)
+GSPC_adj <- GSPC$GSPC.Adjusted
+#eGARCH, ARIMA (0,0,1) sstd
+IXIC <- getSymbols("^IXIC", src = "yahoo", from = "2002-01-01", auto.assign = FALSE)
+IXIC_adj <- IXIC$IXIC.Adjusted
+#eGARCH,ARIMA (0,0,1) sstd
+
+
+BTC <- getSymbols("BTC-USD", src = "yahoo", auto.assign = FALSE)
+BTC_adj <- BTC$`BTC-USD.Adjusted
+#sGARCH, ARIMA(1,0,0) sged`
+ETH <- getSymbols("ETH-USD", src = "yahoo", auto.assign = FALSE)
+ETH_adj <- ETH$`ETH-USD.Adjusted`
+#eGARCH, ARIMA(1,1,1) sged
+XRP <- getSymbols("XRP-USD", src = "yahoo", auto.assign = FALSE)
+XRP_adj <- XRP$`XRP-USD.Adjusted`
+#sGARCH, ARIMA (1,0,3) sged
+
+ret_DJI <- dailyReturn(DJI_adj, type = "log")
+ret_GSPC <- dailyReturn(GSPC_adj, type = "log")
+ret_IXIC <- dailyReturn(IXIC_adj, type = "log")
+ret_BTC <- dailyReturn(BTC_adj, type = "log")
+ret_ETH <- dailyReturn(ETH_adj, type = "log")
+ret_XRP <- dailyReturn(XRP_adj, type = "log")
+#determining qplots
+thing = ret_XRP
+qplot(x = 1:length(thing), y = thing, geom = "line") + geom_line(color = 'yellow') +
+  labs( x = '', y = 'Returns', title =  "DOW JONES") + geom_hline(yintercept = mean(thing), color = 'red')
+
+
+p1 = qplot(x = 1:length(thing) , y = thing , geom = 'line') + geom_line(color = 'darkblue') +
+    geom_hline(yintercept = mean(thing) , color = 'red' , size = 1) +
+    labs(x = '' , y = 'Daily Returns')
+
+p2 = qplot(thing , geom = 'density') + coord_flip() + geom_vline(xintercept = mean(thing) , color = 'red' , size = 1) +
+    geom_density(fill = 'lightblue' , alpha = 0.4) + labs(x = '')
+
+grid.arrange(p1 , p2 , ncol = 2)
+
+adf.test(thing)
+
+model.arima = auto.arima(thing)
+model.arima
+
+model.arima$residuals %>% ggtsdisplay(plot.type = 'hist' , lag.max = 14)
+
+ar.res = model.arima$residuals
+try = arima(thing, c(2,0,1))
+Box.test(model.arima$residuals , lag = 14 , fitdf = 2 , type = 'Ljung-Box')
+coeftest(try)
+coeftest(model.arima)
+model.arima = try
+
+model_residuals <- residuals(model.arima)
+ArchTest(model_residuals - mean(model_residuals))
+
+
+
+model.spec = ugarchspec(variance.model = list(model = 'sGARCH' , garchOrder = c(1 , 1)) ,
+                        mean.model = list(armaOrder = c(1 , 0, 3), include.mean = TRUE), distribution.model = "sged")
+
+(model.fit = ugarchfit(spec = model.spec , data = thing , solver = 'solnp'))
+
--- Project-script.R
+++ Project-script.R
@@ -1,13 +1,107 @@
-# Title     : TODO
-# Objective : TODO
-# Created by: pedro
-# Created on: 03/09/20
 
-pacman::p_load(quantmod, rugarch, rmgarch)
+pacman::p_load(forecast,quantmod, rugarch, rmgarch,coinmarketcapr,xts, tidyverse, ggthemes,
+               gridExtra, tseries, lmtest, FinTS)
 
 DJI <- getSymbols("DJI", src = "yahoo", from = "2002-01-01", auto.assign = FALSE)
 DJI_adj <- DJI$DJI.Adjusted
+#eGARCH, ARIMA (1,0,0) sstd
 GSPC <- getSymbols("^GSPC", src = "yahoo", from = "2002-01-01", auto.assign = FALSE)
 GSPC_adj <- GSPC$GSPC.Adjusted
+#eGARCH, ARIMA (0,0,1) sstd
 IXIC <- getSymbols("^IXIC", src = "yahoo", from = "2002-01-01", auto.assign = FALSE)
 IXIC_adj <- IXIC$IXIC.Adjusted
+#eGARCH,ARIMA (0,0,1) sstd
+
+
+BTC <- getSymbols("BTC-USD", src = "yahoo", auto.assign = FALSE)
+BTC_adj <- BTC$`BTC-USD.Adjusted
+#sGARCH, ARIMA(1,0,0) sged`
+ETH <- getSymbols("ETH-USD", src = "yahoo", auto.assign = FALSE)
+ETH_adj <- ETH$`ETH-USD.Adjusted`
+#eGARCH, ARIMA(1,1,1) sged
+XRP <- getSymbols("XRP-USD", src = "yahoo", auto.assign = FALSE)
+XRP_adj <- XRP$`XRP-USD.Adjusted`
+#sGARCH, ARIMA (1,0,3) sged
+
+ret_DJI <- dailyReturn(DJI_adj, type = "log")
+ret_GSPC <- dailyReturn(GSPC_adj, type = "log")
+ret_IXIC <- dailyReturn(IXIC_adj, type = "log")
+ret_BTC <- dailyReturn(BTC_adj, type = "log")
+ret_ETH <- dailyReturn(ETH_adj, type = "log")
+ret_XRP <- dailyReturn(XRP_adj, type = "log")
+
+
+
+
+rDJI_rBTC <- merge(ret_DJI, ret_BTC, join = "inner")
+rDJI_rETH <- merge(ret_DJI, ret_ETH, join = 'inner')
+rDJI_rXRP <- merge(ret_DJI, ret_XRP, join = 'inner')
+
+rGSPC_rBTC <- merge(ret_GSPC, ret_BTC, join = 'inner')
+rGSPC_rETH <- merge(ret_GSPC, ret_ETH, join = 'inner')
+rGSPC_rXRP <- merge(ret_GSPC, ret_XRP, join = 'inner')
+
+rIXIC_rBTC <- merge(ret_IXIC, ret_BTC, join = 'inner')
+rIXIC_rETH <- merge(ret_IXIC, ret_ETH, join = 'inner')
+rIXIC_rXRP <- merge(ret_IXIC, ret_XRP, join = 'inner')
+
+
+
+
+#DJI vs BTC
+
+uspec.n = multispec(list(ugarchspec(variance.model = list(model = 'eGARCH', garchOrder = c(1,1)),
+                                     mean.model = list(armaOrder = c(1,0,0), include.mean = TRUE), distribution.model = 'sstd'),
+                         ugarchspec(variance.model = list(model = 'eGARCH', garchOrder = c(1,1)),
+                                     mean.model = list(armaOrder = c(1,0,0), include.mean = TRUE), distribution.model = 'sged')))
+(multf = multifit(uspec.n, rDJI_rBTC))
+spec1 = dccspec(uspec = uspec.n, dccOrder = c(1, 1), distribution = 'mvnorm')
+(fit1 = dccfit(spec1, data = rDJI_rBTC, fit.control = list(eval.se = TRUE), fit = multf))
+cov1 = rcov(fit1)  # extracts the covariance matrix
+cor1 = rcor(fit1)  # extracts the correlation matrix
+dim(cor1)
+cor1[,,dim(cor1)[3]]
+cor_DJI_BTC <- cor1[1,2,]   # leaving the last dimension empty implies that we want all elements
+cor_DJI_BTC <- as.xts(cor_DJI_BTC)  # imposes the xts time series format - useful for plotting
+
+plot(cor_DJI_BTC)
+
+
+#DJI vs ETH
+
+
+uspec.n = multispec(list(ugarchspec(variance.model = list(model = 'sGARCH', garchOrder = c(1,1)),
+                                     mean.model = list(armaOrder = c(1,0,0), include.mean = TRUE), distribution.model = 'sged'),
+                         ugarchspec(variance.model = list(model = 'eGARCH', garchOrder = c(1,1)),
+                                     mean.model = list(armaOrder = c(1,1,1), include.mean = TRUE), distribution.model = 'sged')))
+(multf = multifit(uspec.n, rDJI_rETH))
+spec1 = dccspec(uspec = uspec.n, dccOrder = c(1, 1), distribution = 'mvnorm')
+(fit1 = dccfit(spec1, data = rDJI_rETH, fit.control = list(eval.se = TRUE), fit = multf))
+cov1 = rcov(fit1)  # extracts the covariance matrix
+cor1 = rcor(fit1)  # extracts the correlation matrix
+dim(cor1)
+cor1[,,dim(cor1)[3]]
+cor_DJI_ETH <- cor1[1,2,]   # leaving the last dimension empty implies that we want all elements
+cor_DJI_ETH <- as.xts(cor_DJI_ETH)  # imposes the xts time series format - useful for plotting
+
+plot(cor_DJI_ETH)
+
+
+#DJI vs XRP
+
+uspec.n = multispec(list(ugarchspec(variance.model = list(model = 'eGARCH', garchOrder = c(1,1)),
+                                     mean.model = list(armaOrder = c(1,0,0), include.mean = TRUE), distribution.model = 'sstd'),
+                         ugarchspec(variance.model = list(model = 'sGARCH', garchOrder = c(1,1)),
+                                     mean.model = list(armaOrder = c(1,0,3), include.mean = TRUE), distribution.model = 'sged')))
+(multf = multifit(uspec.n, rDJI_rXRP))
+spec1 = dccspec(uspec = uspec.n, dccOrder = c(1, 1), distribution = 'mvnorm')
+(fit1 = dccfit(spec1, data = rDJI_rXRP, fit.control = list(eval.se = TRUE), fit = multf))
+cov1 = rcov(fit1)  # extracts the covariance matrix
+cor1 = rcor(fit1)  # extracts the correlation matrix
+dim(cor1)
+cor1[,,dim(cor1)[3]]
+cor_DJI_XRP <- cor1[1,2,]   # leaving the last dimension empty implies that we want all elements
+cor_DJI_XRP <- as.xts(cor_DJI_XRP)  # imposes the xts time series format - useful for plotting
+
+plot(cor_DJI_XRP)
+
